1296433abad3c9afde58d9b2043cf02219a9309d
ADMIN <buixuanha39@gmail.com>
2021-12-22 17:44:04 +0700

    22/12

--- footer.php
+++ footer.php
@@ -1,14 +1,14 @@
-<footer>
+<footer data-aos="fade-up" data-aos-duration="2000">
     <div class="container">
         <div class="ft-flex">
-            <a href="" title="" class="ft-logo"><img src="images/logo.png" alt="" data-aos="fade-up" data-aos-duration="2000"> </a>
-            <div class="ft-social flex-center-center" data-aos="fade-up" data-aos-duration="2000">
+            <a href="" title="" class="ft-logo"><img src="images/logo.png" alt=""> </a>
+            <div class="ft-social flex-center-center" >
                 <a href="" title="" class="fab fa-facebook-f"></a>
                 <a href="" title="" class="fab fa-linkedin-in"></a>
                 <a href="" title="" class="fab fa-instagram"></a>
                 <a href="" title="" class="fab fa-twitter"></a>
             </div>
-            <span class="copyright" data-aos="fade-up" data-aos-duration="2000">©All rights reserved Sun Group</span>
+            <span class="copyright">©All rights reserved Sun Group</span>
         </div>
     </div>
 </footer>
