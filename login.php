9c4629f6a660a262c0e6feb2a0b429bce0ac11a7
ADMIN <buixuanha39@gmail.com>
2021-12-23 23:22:24 +0700

    23/12

---
+++ login.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Trang chủ | </title>
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
+    <meta name="author" content="" />
+    <meta name="copyright" content="" />
+    <meta property="og:image" content="">
+    <!--link css-->
+    <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css"/>
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/jquery.fancybox.min.css">
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
+    <link rel="stylesheet" type="text/css" href="css/responsive.css">
+    <script type="text/javascript" src="js/jquery.min.js"></script>
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+</head>
+<body>
+<main class="login">
+    <section class="login-box">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 offset-md-3">
+                    <div class="login-box">
+                        <div class="login-inner">
+                            <p><a href="" title=""><img src="images/logo.png" alt=""> </a> </p>
+                            <h4>Đăng nhập Nhật ký lỗi</h4>
+                            <form class="login-frm">
+                                <div class="frm-input">
+                                    <input type="email" placeholder="Địa chỉ email của bạn">
+                                    <span><i class="fal fa-envelope"></i> </span>
+                                </div>
+                                <div class="frm-input">
+                                    <input type="password" placeholder="Mật khẩu">
+                                    <span><i class="fal fa-lock"></i></span>
+                                </div>
+                                <button type="submit">Đăng nhập</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</main>
+<!--Link js-->
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/jquery.fancybox.min.js"></script>
+<script type="text/javascript" src="js/private.js"></script>
+</body>
+</html>
---
+++ pj-intro.php
@@ -0,0 +1,82 @@
+<?php include 'header.php' ?>
+<main>
+    <section class="banner relative">
+        <div class="banner-social">
+            <div class="social">
+                <a href="" title="" class="fab fa-instagram"></a>
+                <a href="" title="" class="fab fa-twitter"></a>
+                <a href="" title="" class="fab fa-linkedin-in"></a>
+                <a href="" title="" class="fab fa-facebook-f"></a>
+            </div>
+        </div>
+        <div class="banner-i relative">
+            <img src="images/banner-page.jpg" alt="">
+            <div class="banner-page">
+                <div class="container">
+                    <h1 class="text-right">giới thiệu dự án</h1>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="pj-intro">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8">
+                    <div class="pj-content">
+                        <h1>DỰ ÁN CẢNG TÀU KHÁCH QUỐC TẾ</h1>
+                        <div class="flex-center-between mgb-30">
+                            <div class="date"><i class="fal fa-calendar"></i> 16.06.2021</div>
+                            <div class="share flex-center-end">
+                                <span>Chia sẻ bài viết này</span>
+                                <a href="" title="" class="fab fa-instagram"></a>
+                                <a href="" title="" class="fab fa-twitter"></a>
+                                <a href="" title="" class="fab fa-linkedin-in"></a>
+                                <a href="" title="" class="fab fa-facebook-f"></a>
+                            </div>
+                        </div>
+                        <div class="pj-article">
+                            <p>Tọa lạc tại trung tâm Bãi Cháy, gần tổ hợp Sun World Halong Complex, đây là công trình giao thông bến cảng cấp đặc biệt và là cảng tàu khách du lịch quốc tế chuyên biệt đầu tiên của Việt Nam</p>
+                            <p><img src="images/image-3.jpg" alt=""> </p>
+                            <p>Tọa lạc tại trung tâm Bãi Cháy, gần tổ hợp Sun World Halong Complex, đây là công trình giao thông bến cảng cấp đặc biệt và là cảng tàu khách du lịch quốc tế chuyên biệt đầu tiên của Việt Nam</p>
+                            <p>Tọa lạc tại trung tâm Bãi Cháy, gần tổ hợp Sun World Halong Complex, đây là công trình giao thông bến cảng cấp đặc biệt và là cảng tàu khách du lịch quốc tế chuyên biệt đầu tiên của Việt Nam</p>
+                            <p>Tọa lạc tại trung tâm Bãi Cháy, gần tổ hợp Sun World Halong Complex, đây là công trình giao thông bến cảng cấp đặc biệt và là cảng tàu khách du lịch quốc tế chuyên biệt đầu tiên của Việt Nam</p>
+                            <p>Tọa lạc tại trung tâm Bãi Cháy, gần tổ hợp Sun World Halong Complex, đây là công trình giao thông bến cảng cấp đặc biệt và là cảng tàu khách du lịch quốc tế chuyên biệt đầu tiên của Việt Nam</p>
+                            <ul>
+                                <li><span>Website:</span>  http://www.halongport.vn</li>
+                                <li><span>Hotline:</span> +84 328 225 699</li>
+                                <li><span>Địa chỉ:</span> Phường Bãi Cháy, TP Hạ Long, Tỉnh Quảng Ninh</li>
+                                <li><span>Khởi công:</span> 2017</li>
+                                <li><span> Hoàn thành:</span> 2018</li>
+                                <li><span>Tổng giá trị đầu tư:</span> 1.100 tỷ đồng</li>
+                                <li><span>Quy mô:</span> 200ha</li>
+                                <li><span>Địa chỉ:</span> Phường Bãi Cháy, TP Hạ Long, Tỉnh Quảng Ninh</li>
+                                <li><span>Khởi công:</span> 2017</li>
+                                <li><span>Hoàn thành:</span> 2018</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="sticky-top">
+                        <div class="pj-related">
+                            <h3>dự án KHÁC</h3>
+                            <div class="related-item">
+                                <a href="" title="" class="zoom"><img src="images/image-3.jpg" alt=""> </a>
+                                <h4><a href="" title="">SUN WORLD HALONG COMPLEX</a> </h4>
+                            </div>
+                            <div class="related-item">
+                                <a href="" title="" class="zoom"><img src="images/image-3.jpg" alt=""> </a>
+                                <h4><a href="" title="">SUN WORLD HALONG COMPLEX</a> </h4>
+                            </div>
+                            <div class="related-item">
+                                <a href="" title="" class="zoom"><img src="images/image-3.jpg" alt=""> </a>
+                                <h4><a href="" title="">SUN WORLD HALONG COMPLEX</a> </h4>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</main>
+<?php include 'footer.php' ?>
