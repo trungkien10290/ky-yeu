95abc0fa582a2b7cd98205ea0a76b08d8a956c20
dungnt <dev.dungnt1312@gmail.com>
2022-01-03 15:39:18 +0700

    fix phpcs

--- config/filesystems.php
+++ config/filesystems.php
@@ -37,8 +37,9 @@ return [
 
         'admin' => [
             'driver' => 'local',
-            'root' => public_path('uploads/'),
-            'url' => env('APP_URL') . '/uploads',
+            'root' => storage_path('app/public'),
+            'url' => env('APP_URL') . '/storage',
+            'visibility' => 'public',
         ],
         'public' => [
             'driver' => 'local',
